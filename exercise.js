1dcba19f033761cb181b6bcb22856ea20a28d808
Adolfo-97 <adolfogomez@live.it>
2023-09-23 15:15:11 +0200

    Build1: upload esercizio

--- exercise.js
+++ exercise.js
@@ -18,13 +18,23 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  static calculator(figure) {
+    if (figure instanceof Square) {
+      return figure.side ** 2; // Area of a square
+    } else if (figure instanceof Rectangle) {
+      return figure.width * figure.height; // Area of a rectangle
+    } else if (figure instanceof Circle) {
+      return 2 ** Math.PI * figure.radius; // Area of a circle
+    } else {
+      throw new Error("Unsupported figure");
+    }
+  }
 }
 
 const square = new Square(4);
 const rectangle = new Rectangle(4, 2);
 const circle = new Circle(5);
 
-console.log(AreaCalculator.calculate(square));
-console.log(AreaCalculator.calculate(rectangle));
-console.log(AreaCalculator.calculate(circle));
+console.log(AreaCalculator.calculator(square));
+console.log(AreaCalculator.calculator(rectangle));
+console.log(AreaCalculator.calculator(circle));
