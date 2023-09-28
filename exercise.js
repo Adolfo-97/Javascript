b9eabe314a7df661ed1f68fa46a580ce16dd2862
Adolfo-97 <adolfogomez@live.it>
2023-09-28 09:24:56 +0200

    Build1: Upload esercizio

---
+++ exercise.js
@@ -0,0 +1,22 @@
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+const id = person.id;
+const personInfo = {
+  firstName: person.firstName,
+  lastName: person.lastName,
+  age: person.age,
+};
+
+/* console.log(id, personInfo);
+ */
+
+function refactoring(id, personInfo) {
+  console.log(id, { ...personInfo });
+}
+
+refactoring(id, personInfo);
