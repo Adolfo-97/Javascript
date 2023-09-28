76b965097efa75c0a17dfef2b07837602219db64
Adolfo-97 <adolfogomez@live.it>
2023-09-28 10:49:35 +0200

    Upload esercizio

---
+++ exercise.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  static fromJson(json) {
+    const result = JSON.parse(json);
+    return new Person(result.id, result.firstName, result.lastName, result.age);
+  }
+  toJson() {
+    return JSON.stringify(this);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
