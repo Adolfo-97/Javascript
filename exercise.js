4f6fa3b43fe79523e3980024cc9c5b8059e73a1a
Adolfo-97 <adolfogomez@live.it>
2023-09-28 15:40:33 +0200

    Upload esercizio

---
+++ exercise.js
@@ -0,0 +1,19 @@
+const person1 = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+  address: {
+    state: "Italy",
+    city: "Rome",
+    street: "Via Romano, 12",
+  },
+};
+
+const person1toJSON = JSON.stringify(person1);
+const person2 = JSON.parse(person1toJSON);
+
+person2.address.city = "Milan";
+
+console.log(person1);
+console.log(person2);
