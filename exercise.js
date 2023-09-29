e9d183b6d8d77a174a978d5937059e4fa79ce765
Adolfo-97 <adolfogomez@live.it>
2023-09-29 12:27:55 +0200

    Fix esercizio

--- exercise.js
+++ exercise.js
@@ -1,9 +1,5 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-// numberStore.push(newNumber);
 
-function merge(newNumber, numberStore) {
-  let merged = [...numberStore, newNumber];
-  return merged;
-}
-console.log(merge(newNumber, numberStore));
+numberStore = [...numberStore, newNumber];
+console.log(numberStore);
