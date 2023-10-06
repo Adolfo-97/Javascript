fbca991aeb407e3960681733f8990927ac168508
Adolfo-97 <adolfogomez@live.it>
2023-10-06 16:50:08 +0200

    Upload esercizio

---
+++ exercise.js
@@ -0,0 +1,9 @@
+function printAsyncName(name, callback) {
+  setTimeout(callback, 1000);
+  setTimeout(() => console.log(name), 2000);
+}
+
+const sayHello = () => console.log("Hello");
+
+printAsyncName("Mario", sayHello);
+// Esercizio in pair
