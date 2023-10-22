54a11e8918b30550d595a99d46da41184315e8ee
Juan Manuel Gomez Giraldo <adolfogomez@live.it>
2023-10-22 17:32:12 +0200

    upload

--- exercise.js
+++ exercise.js
@@ -3,3 +3,21 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveUser(userToJSON) {
+  try {
+    localStorage.setItem("user",JSON.stringify(userToJSON));
+    //console.log(userToJSON)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+function loadUser(userToString){
+  let user = localStorage.getItem(userToString)
+  console.log(user);
+}
+saveUser(user);
+
+loadUser("user")
